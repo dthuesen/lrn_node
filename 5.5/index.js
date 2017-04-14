ea50e4dcf86c42779f580f8774ad902a6b92e0b0
Detlef von der Thuesen <dthuesen@gmx.de>
2017-04-14 17:39:14 +0200

    5.5

---
+++ 5.5/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const contactRouter = require('./contact-router');
+
+app.use('/v1/contact', contactRouter);
+
+app.listen(8000);
