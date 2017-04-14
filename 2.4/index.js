53f7ba428d9989e430244c81b4b2705193415bb4
Detlef von der Thuesen <dthuesen@gmx.de>
2017-04-14 17:31:59 +0200

    2.4

---
+++ 2.4/index.js
@@ -0,0 +1,22 @@
+const express     = require('express');
+const app         = express(); 
+const port        = 8080;
+const url         = require('url');
+const morgan      = require('morgan');
+
+app.use(morgan('short'));
+
+app.get('/', (req, res) => {
+    const queryObject = url.parse(req.url, true).query;
+    const greeting = queryObject.greeting || 'Default greeting';
+    res.send('The Home Page ' + greeting);
+})      
+
+app.get('/greet/:statement', (req, res) => {
+    const greeting = req.params.statement || 'Default greeting under /greeting';
+    res.send('This is the greet url ' + greeting);
+}) 
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
